371b359d6fb585166f43744c67d14bc76a6137bd
asanda8106 <ngwenyaasanda13@gmail.com>
2025-01-08 12:31:20 +0200

    created the pages and initial styling

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// JavaScript for smooth scrolling when clicking navigation links
+document.querySelectorAll('.icon-bar a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
